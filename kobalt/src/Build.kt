4171f5bb39fe4a87a8c28c5a29784a9fd36ad12f
Erik C. Thauvin <erik@thauvin.net>
2017-03-08 11:19:40 -0800

    Kobalt 1.0.6 update.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -12,16 +12,18 @@ import org.apache.maven.model.Developer
 import org.apache.maven.model.License
 import org.apache.maven.model.Model
 import org.apache.maven.model.Scm
+import java.io.File
 
 val semver = "0.6.3"
 
 val bs = buildScript {
-    val f = java.io.File("kobaltBuild/libs/kobalt-exec-$semver.jar")
-    val p = if (f.exists()) {
-        kobaltLog(1, "  >>> Using: ${f.path}")
-        file(f.path)
-    } else {
-        "net.thauvin.erik:kobalt-exec:"
+    val p = with(File("kobaltBuild/libs/kobalt-exec-$semver.jar")) {
+        if (exists()) {
+            kobaltLog(1, "  >>> Using: $path")
+            file(path)
+        } else {
+            "net.thauvin.erik:kobalt-exec:"
+        }
     }
     plugins("net.thauvin.erik:kobalt-versioneye:", p)
 }
