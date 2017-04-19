c132f817b4fc9cf8c244a2047b54841950cedb46
Erik C. Thauvin <erik@thauvin.net>
2017-04-18 17:58:33 -0700

    Kobalt 1.0.69 update.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -1,25 +1,13 @@
-import com.beust.kobalt.*
-import com.beust.kobalt.misc.*
-import com.beust.kobalt.plugin.application.*
-import com.beust.kobalt.plugin.packaging.*
-import com.beust.kobalt.plugin.publish.*
-import net.thauvin.erik.kobalt.plugin.exec.*
-import net.thauvin.erik.kobalt.plugin.versioneye.*
-import org.apache.maven.model.*
-import java.io.*
 
-val semver = "0.6.3"
+import com.beust.kobalt.*
+import com.beust.kobalt.plugin.packaging.assemble
+import com.beust.kobalt.plugin.publish.autoGitTag
+import com.beust.kobalt.plugin.publish.bintray
+import net.thauvin.erik.kobalt.plugin.versioneye.versionEye
+import org.apache.maven.model.*
 
 val bs = buildScript {
-    val p = with(File("kobaltBuild/libs/kobalt-exec-$semver.jar")) {
-        if (exists()) {
-            kobaltLog(1, "  >>> Using: $path")
-            file(path)
-        } else {
-            "net.thauvin.erik:kobalt-exec:"
-        }
-    }
-    plugins("net.thauvin.erik:kobalt-versioneye:", p)
+    plugins("net.thauvin.erik:kobalt-versioneye:")
 }
 
 val dev by profile()
@@ -30,7 +18,7 @@ val p = project {
     name = "kobalt-exec"
     group = "net.thauvin.erik"
     artifactId = name
-    version = semver
+    version = "0.6.4"
 
     pom = Model().apply {
         description = "Command Line Execution plug-in for the Kobalt build system."
@@ -57,7 +45,6 @@ val p = project {
 
     dependenciesTest {
         compile("org.testng:testng:")
-
     }
 
     assemble {
@@ -78,42 +65,5 @@ val p = project {
     versionEye {
         org = "Thauvin"
         team = "Owners"
-    }
-}
-
-val example = project(p) {
-
-    name = "example"
-    group = "com.example"
-    artifactId = name
-    version = "0.1"
-    directory = ("example")
-
-    assemble {
-        jar {
-        }
-    }
-
-    application {
-        mainClass = "com.example.Main"
-    }
-
-    exec {
-        commandLine(listOf("echo", "Test Example 1"), os = setOf(Os.LINUX))
-        commandLine(listOf("cmd", "/c", "echo", "Test Example 1"), os = setOf(Os.WINDOWS))
-        commandLine(args = listOf("ls", "-l"), dir = "../libs", os = setOf(Os.LINUX))
-        commandLine(args = listOf("cmd", "/c", "dir /Q"), dir = "../libs", os = setOf(Os.WINDOWS))
-    }
-}
-
-val example2 = project(p) {
-    name = "example2"
-    directory = "example"
-
-    exec {
-        commandLine(listOf("cmd", "/c", "echo", "Test Example 2"), os = setOf(Os.WINDOWS))
-        commandLine(listOf("echo", "Test example 2"), os = setOf(Os.LINUX))
-        //commandLine(listOf("cmd", "/c", "tasklist | find \"cmd.exe\""), os = setOf(Os.WINDOWS), fail = setOf(Fail.NONE))
-        commandLine(listOf("/bin/sh", "-c", "ps aux | grep bash"), os = setOf(Os.LINUX))
     }
 }
